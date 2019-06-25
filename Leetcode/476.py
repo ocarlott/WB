aaa2af503308867aa4d019133936371e1d63cfc7
ocarlott <ocarlott@users.noreply.github.com>
2019-06-24 23:19:55 -0500

    Number Complement

--- Leetcode/476.py
+++ Leetcode/476.py
@@ -1,7 +1,7 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         mask = 0
-        for i in range(33): # O(logn). Try to find correct mask
+        for i in range(32): # O(logn). Try to find correct mask
             mask += 2 ** i
             if num <= mask:
                 break
