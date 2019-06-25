4d81a34dd87709fefbe6a3ade03de7822244e8d4
ocarlott <ocarlott@users.noreply.github.com>
2019-06-24 23:55:50 -0500

    Number Complement

--- Leetcode/476.py
+++ Leetcode/476.py
@@ -2,7 +2,7 @@ class Solution:
     def findComplement(self, num: int) -> int:
         mask = 0
         for i in range(32): # O(Logn). Try to find correct mask
-            mask += 2 ** i # O(LogI). Log(1) + Log(2) + .. + Log(N) < NLogN. Can precompute this to make this an O(LogN) algo.
+            mask += 1 << i # O(LogI). Log(1) + Log(2) + .. + Log(N) < NLogN. Can precompute this to make this an O(LogN) algo.
             if num <= mask:
                 break
         return ~num & mask # O(LogN)
