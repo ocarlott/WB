cc5d72759dd4aa3436195d5b0cb4d6a2d3b6cd66
ocarlott <ocarlott@users.noreply.github.com>
2019-06-24 19:30:27 -0500

    Valid Palindrome

---
+++ Leetcode/125.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        length = len(s)
+        right = length - 1
+        while True:
+            if right - left <= 0:
+                return True
+            while not s[left].isalnum():
+                left += 1
+                if left == length:
+                    return True
+            while not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+O(n) for time. O(1) for space.
