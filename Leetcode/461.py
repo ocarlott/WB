aab5b98cba3485f5178dbbc3a2400a4f3c8074ef
ocarlott <ocarlott@users.noreply.github.com>
2019-06-24 23:42:22 -0500

    Update 461.py

--- Leetcode/461.py
+++ Leetcode/461.py
@@ -2,7 +2,7 @@ class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         total = 0
         for i in range(32): # O(LogN)
-            bitMask = 2 ** i # O(LogI)
+            bitMask = 2 ** i # O(LogI). Can precompute this to list to make this a O(LogN) algorithm
             if bitMask > x and bitMask > y:
                 break
             if (bitMask & x) != (bitMask & y):
