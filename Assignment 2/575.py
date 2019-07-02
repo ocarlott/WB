ae812985bcff8c88628fbd39999a8ae4afdb4ff4
ocarlott <ocarlott@users.noreply.github.com>
2019-07-02 16:46:32 -0500

    Distribute Candies

--- Assignment 2/575.py
+++ Assignment 2/575.py
@@ -1,8 +1,8 @@
 class Solution:
     def distributeCandies(self, candies: List[int]) -> int:
-        count = {}
+        kinds = set()
         for candy in candies: # O(N)
-            count[candy] = count.get(candy, 0) + 1
-        return min(len(count.keys()), len(candies) // 2)
+            kinds.add(candy)
+        return min(len(kinds), len(candies) // 2)
 
 # O(N) for time and space.
