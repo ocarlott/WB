02d600024d05cfd2ebebabd61e298e669d0b333f
ocarlott <ocarlott@users.noreply.github.com>
2019-07-02 16:19:52 -0500

    Groups of Special-Equivalent Strings

--- Assignment 2/893.py
+++ Assignment 2/893.py
@@ -4,7 +4,7 @@ class Solution:
         for word in A: # { word: (even_set(), odd_set()) } # O(N)
             wEven = word[::2] # O(M) for time and space
             wOdd = word[1::2]  # O(M)
-            wordSet.add((''.join(sorted(wEven)), "".join(sorted(wOdd)))) # sorted takes O(MLog(M)), join takes O(M) and add takes O(1). These 3 operations work sequentially. Therefore, the dominant term is O(MLog(M))
+            wordSet.add((frozenset(wEven), frozenset(wOdd))) # Convert list to frozenset takes O(M) and add takes O(1).
         return len(wordSet)
     
-# O(NMLog(M)) for time. O(N + M) for space.
+# O(NM) for time. O(N + M) for space.
