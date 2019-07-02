4fb6fd0939fbcd7dba277227ff453d246b41df6d
ocarlott <ocarlott@users.noreply.github.com>
2019-07-02 16:32:47 -0500

    Unique Morse Code Words

--- Assignment 2/804.py
+++ Assignment 2/804.py
@@ -1,11 +1,12 @@
 import string
+from collections import defaultdict
 
 lowercase = string.ascii_lowercase
 
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        freq = {}
+        codeSet = set()
         codeDict = {}
         for i in range(26):
             codeDict[lowercase[i]] = codes[i]
@@ -13,7 +14,7 @@ class Solution:
             wordTransformed = ""
             for c in word:  # O(M)
                 wordTransformed += codeDict[c]
-            freq[wordTransformed] = freq.get(wordTransformed, 0)
-        return len(freq.keys())
+            codeSet.add(wordTransformed)
+        return len(codeSet)
 
 # O(NM) for time. O(N) for space
