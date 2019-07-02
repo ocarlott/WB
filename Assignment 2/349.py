74a63596c5b52353d98d05b0f5e2f3741e5cda15
ocarlott <ocarlott@users.noreply.github.com>
2019-07-02 16:54:41 -0500

    Intersection of Two Arrays

--- Assignment 2/349.py
+++ Assignment 2/349.py
@@ -1,10 +1,8 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         setNums1 = set(nums1) # O(N) for time and space
-        result = set() # O(M) for space
-        for n in nums2: # O(M)
-            if n in setNums1:
-                result.add(n)
+        setNums2 = set(nums2) # O(M) for space
+        result = setNums1.intersection(setNums2)
         return list(result) # O(M)
 
 
