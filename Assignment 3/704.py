4a30c0feb3d44900b168f625656a05624e8e81d1
ocarlott <ocarlott@users.noreply.github.com>
2019-07-08 19:54:48 -0500

    Binary Search

---
+++ Assignment 3/704.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        if nums[left] == target:
+            return left
+        if nums[right] == target:
+            return right
+        mid = (left + right) // 2
+        while True:
+            if nums[mid] == target:
+                return mid
+            if left == mid or right == mid:
+                return -1
+            if nums[mid] > target:
+                right = mid
+            elif nums[mid] < target:
+                left = mid
+            mid = (left + right) // 2
+# O(N) for time. O(1) for space.
