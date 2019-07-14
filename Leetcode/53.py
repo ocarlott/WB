1b031155d475f3b0351d23ec535977974a56e73b
ocarlott <ocarlott@users.noreply.github.com>
2019-07-13 23:43:22 -0500

    Maximum Subarray

--- Leetcode/53.py
+++ Leetcode/53.py
@@ -2,12 +2,10 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        sums = [nums[0]]
+        globalMax = localMax = nums[0]
         for i in range(1, len(nums)):
-            sums.append(max(nums[i] + sums[i - 1], nums[i]))
-        result = nums[0]
-        for n in sums:
-            if n > result:
-                result = n
-        return result
-# O(N) for time and space.
+            localMax = max(localMax + nums[i], nums[i])
+            if localMax > globalMax:
+                globalMax = localMax
+        return globalMax
+# O(N) for time. O(1) for space.
