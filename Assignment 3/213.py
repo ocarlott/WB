a10bc34de1efdb73f82163cfbea842cf4fd3d335
ocarlott <ocarlott@users.noreply.github.com>
2019-07-14 12:54:37 -0500

    House Robber II

--- Assignment 3/213.py
+++ Assignment 3/213.py
@@ -19,3 +19,25 @@ class Solution:
             return max(f, s)
 
 # O(N) for time. O(1) for space       
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def calc(nums, i, end):
+            if i == end:
+                return (nums[i], 0)
+            value = calc(nums, i - 1, end)
+            withCur = value[1] + nums[i]
+            withoutCur = max(value[0], value[1])
+            return (withCur, withoutCur)
+        
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        else:
+            f = calc(nums, len(nums) - 2, 0)
+            s = calc(nums, len(nums) - 1, 1)
+            return max(f[0], f[1], s[0], s[1])
+
+# O(N) for time. O(1) for space     
