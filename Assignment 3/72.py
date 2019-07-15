55c8e635b3c8cbe33010eeb5f591fad61123ed69
ocarlott <ocarlott@users.noreply.github.com>
2019-07-15 02:07:37 -0500

    Edit Distance

--- Assignment 3/72.py
+++ Assignment 3/72.py
@@ -13,3 +13,30 @@ class Solution:
                 dp[row][col] = min(dp[row - 1][col - 1] + nForSub, dp[row][col - 1] + 1, dp[row - 1][col] + 1)
         return dp[l2][l1]
 # O(NM) for time and space.
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1 = len(word1)
+        l2 = len(word2)
+        cache = { (0, 0): 0 }
+        for i in range(1, l1 + 1):
+            cache[(i, 0)] = i
+        for j in range(1, l2 + 1):
+            cache[(0, j)] = j
+            
+        def getDistance(word1, word2, i, j):
+            if (i + 1, j + 1) in cache:
+                return cache[(i + 1, j + 1)]
+            d = 0
+            if word1[i] == word2[j]:
+                d =  getDistance(word1, word2, i - 1, j - 1)
+            else:
+                delete = getDistance(word1, word2, i - 1, j) + 1
+                insert = getDistance(word1, word2, i, j - 1) + 1
+                swap = getDistance(word1, word2, i - 1, j - 1) + 1
+                d = min(delete, insert, swap)
+            cache[(i + 1, j + 1)] = d
+            return d
+        return getDistance(word1, word2, l1 - 1, l2 - 1)
+            
+# O(NM) for time and space.
