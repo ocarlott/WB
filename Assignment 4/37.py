a5f27da9460983dca6bfa5ee7a3a20c82c837a35
ocarlott <ocarlott@users.noreply.github.com>
2019-07-23 23:23:40 -0500

    Sudoku Solver

--- Assignment 4/37.py
+++ Assignment 4/37.py
@@ -5,44 +5,46 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        empties = set()
-        for r in range(len(board)):
-            for c in range(len(board)):
-                if board[r][c] == ".":
-                    empties.add((r, c))
-        self.solve(board, empties)
-            
-    def solve(self, board, empties):
-        r, c = self.getEmptyCell(board)
-        if r == -1 and c == -1:
-            return True
-        for i in range(len(board)):
-            board[r][c] = st[i]
-            if self.isValid(board, r, c):
-                if self.solve(board):
-                    return True
-            board[r][c] = "."
-        return False
-    
-    def isValid(self, board, row, col):
+        empties = []
         n = len(board)
         m = 1
         while m * m < n:
             m += 1
-        rowSet = set()
-        colSet = set()
-        blockSet = set()
-        for i in range(n):
-            if board[row][i] in rowSet or board[i][col] in colSet:
-                return False
-            if board[row][i] != ".":
-                rowSet.add(board[row][i])
-            if board[i][col] != ".":
-                colSet.add(board[i][col])
-        for i in range((row // m) * m, (row // m) * m + m):
-            for j in range((col // m) * m, (col // m) * m + m):
-                if board[i][j] in blockSet:
-                    return False
-                if board[i][j] != ".":
-                    blockSet.add(board[i][j])
-        return True
+        blockSets = [set() for i in range(n)]
+        rowSets = [set() for i in range(n)]
+        colSets = [set() for i in range(n)]
+        for r in range(n):
+            for c in range(n):
+                cur = board[r][c]
+                if cur == ".":
+                    empties.append((r, c))
+                else:
+                    rowSets[r].add(cur)
+                    colSets[c].add(cur)
+                    blockSets[(r // m) * m + (c // m)].add(cur)
+        self.solve(board, m, empties[::-1], rowSets, colSets, blockSets)
+            
+    def solve(self, board, m, empties, rowSets, colSets, blockSets):
+        if len(empties) == 0:
+            return True
+        for i in range(len(board)):
+            (r, c) = empties.pop()
+            board[r][c] = st[i]
+            blockIndex = (r // m) * m + (c // m)
+            if self.isValid(board, blockIndex, r, c, rowSets, colSets, blockSets):
+                rowSets[r].add(st[i])
+                colSets[c].add(st[i])
+                blockSets[blockIndex].add(st[i])
+                if self.solve(board, m, empties, rowSets, colSets, blockSets):
+                    return True
+                rowSets[r].remove(st[i])
+                colSets[c].remove(st[i])
+                blockSets[blockIndex].remove(st[i])
+            empties.append((r, c))
+            board[r][c] = "."
+        return False
+    
+    def isValid(self, board, blockIndex, row, col, rowSets, colSets, blockSets):
+        temp = board[row][col]
+        return temp not in rowSets[row] and temp not in colSets[col] and temp not in blockSets[blockIndex]
+    
